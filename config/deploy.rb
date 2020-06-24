b19ec4a485cec70f636f4298fffa30146bc8c2ac
YourName <you@example.com>
2020-06-24 19:50:10 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,3 @@
-# # secrets.yml用のシンボリックリンクを追加
-# set :linked_files, %w{ config/secrets.yml }
-
-# # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
-
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-
-#   desc 'upload secrets.yml'
-#   task :upload do
-#     on roles(:app) do |host|
-#       if test "[ ! -d #{shared_path}/config ]"
-#         execute "mkdir -p #{shared_path}/config"
-#       end
-#       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-#     end
-#   end
-#   before :starting, 'deploy:upload'
-#   after :finishing, 'deploy:cleanup'
-# end
+set :application, 'OriginalApp'
+set :repo_url, 'git@github.com:OriginalApp.git'
+set :deploy_to, "/var/www/apps/OriginalApp"
