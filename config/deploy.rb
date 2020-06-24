78c25c7028f85256884c0c7d7b348d569b8c4f65
YourName <you@example.com>
2020-06-24 20:32:11 +0900

    deplpy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,55 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano (3.14.1)'
+# アプリ名、GitHubリポジトリ、ユーザーを設定  
+set :application, 'OriginalApp'  
+set :repo_url, 'git@github.com:0223ms/OriginalApp.git'  
+set :user, 'deploy'  
 
-# Capistranoのログの表示に利用する
-set :application, 'OriginalApp'
+# デプロイ先ディレクトリを指定  
+set :deploy_to, "/srv/#{fetch(:application)}"  
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:0223ms/OriginalApp.git'
+# sudoコマンドを使うために必要  
+set :pty, true  
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# リンクファイルとリンクディレクトリの設定  
+append :linked_files, 'config/credentials/production.key'  
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/packs', 'node_modules'  
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/OriginalApp.pem']  
+# リモートにキャッシュを残して、二回目以降は差分だけ更新する  
+set :deploy_via, :remote_cache  
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# マイグレートファイルに変更がある時だけマイグレートする  
+set :conditionally_migrate, true  
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+# linked_filesに記述してあるものを全てリモートサーバにコピー（今回はproduction.keyだけ）  
+namespace :safe_deploy_to do  
+  task :push_config do  
+    next unless any? :linked_files  
+    on roles(:app) do  
+      fetch(:linked_files).each do |file|  
+        unless test "[ -f #{shared_path.join file} ]"  
+          execute :mkdir, '-p', shared_path.join(File.dirname(file))  
+          upload! file, "#{shared_path.join file}"  
+        end  
+      end  
+    end  
+  end  
+  after 'safe_deploy_to:ensure', 'safe_deploy_to:push_config'  
+end  
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+# rbenvの設定  
+set :rbenv_type, :user  
+set :rbenv_custom_path, '/home/deploy/.anyenv/envs/rbenv'  
+set :rbenv_ruby, File.read('.ruby-version').strip  
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"  
+set :rbenv_map_bins, %w[rake gem bundle ruby rails]  
+set :rbenv_roles, :all  
+
+# nodeの設定  
+set :default_env, { NODE_ENV: 'production' }  
+
+# bundlerの設定  
+append :linked_dirs, '.bundle'  
+set :bundle_jobs, 1  
+
+# # pumaコマンドをbundle execで実行  
+# append :rbenv_map_bins, 'puma', 'pumactl'  
