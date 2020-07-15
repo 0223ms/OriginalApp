d7fa8836a5955e0b362ef01e4ec7576f637823fc
YourName <you@example.com>
2020-07-15 22:45:10 +0900

    database-update

--- db/migrate/20200714085158_create_images.rb
+++ db/migrate/20200714085158_create_images.rb
@@ -3,7 +3,6 @@ class CreateImages < ActiveRecord::Migration[5.2]
     create_table :images do |t|
       t.string :photo, null: false
       t.references :post, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
