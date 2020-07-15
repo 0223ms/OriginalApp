9a4b25c291b8789ce020f1d4e58295dcb5d144af
YourName <you@example.com>
2020-07-15 15:33:02 +0900

    databese-update

--- db/migrate/20200714085158_create_images.rb
+++ db/migrate/20200714085158_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.string :photo, null: false
-      t.integer :content_id, null: false
+      t.integer :post_id, null: false
       t.integer :user_id, null: false
       t.timestamps
     end
