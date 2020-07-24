0bb6f874c4ec68242e48f2be7370505e684c178a
akkun016 <nakataakito@icloud.com>
2020-07-24 19:47:15 +0900

    user-show-front

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
     :sessions => 'users/sessions'
   }
 
-  resources :exhibitions, only: [:index]
+  resources :exhibitions, only: [:index] do
+    collection do
+      get :save_post
+      get :tag_post
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
