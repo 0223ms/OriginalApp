bdc3f9966a33513f8f99eb1b2cab17d0694c6e55
akkun016 <nakataakito@icloud.com>
2020-07-26 22:09:32 +0900

    message追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     end
   end
   root "tops#index"
+
+  resources :messages, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
